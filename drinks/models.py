a0b5a041a3807e7d6038679773b1a962f050b0e4
malek <m.baba@microfin.de>
2022-08-07 16:54:43 +0200

    put, delete and get details endpoints are done

--- drinks/models.py
+++ drinks/models.py
@@ -5,4 +5,4 @@ class Drink(models.Model):
     description = models.CharField(max_length=500)
     
     def __str__(self):
-        return self.name
+        return self.name 
--- drinks/views.py
+++ drinks/views.py
@@ -4,16 +4,41 @@ from .models import Drink
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.response import Response
 
 @api_view(['GET', 'POST'])
 def drink_list(request):
     if request.method == 'GET':
         drinks = Drink.objects.all()
         serializer = DrinkSerializer(drinks, many=True)
-        return JsonResponse({"drinks": serializer.data}, safe=False)
+        return Response({"drinks": serializer.data})
 
     if request.method == 'POST':
         serializer = DrinkSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({"drinks": serializer.data}, safe=False, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+@api_view(['GET', 'PUT', 'DELETE'])   
+def drink_detail(request, pk):
+    try:
+        drink = Drink.objects.get(pk=pk)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
